58cedaa1f7eafd5316e0a535849d62a5405c7a95
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2024-01-13 20:41:17 +0530

    minor fix

--- src/config/connectDB.ts
+++ src/config/connectDB.ts
@@ -7,9 +7,7 @@ const dbURL = process.env.MONGO_URI;
 export default async function connectDB() {
   try {
     await mongoose.connect(dbURL!);
-    console.log(
-      `mongoose connection successfully ${mongoose.connections[0].name}`
-    );
+    console.log(`mongoose connection successfully `);
   } catch (error) {
     console.log("Unable to connect to mongoDB");
     console.log(error);
